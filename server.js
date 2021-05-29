62fbdbc369de0105036fbc61f0acef68744a139e
Mark Artishuk <markyshuk@gmail.com>
2021-05-29 09:19:45 -0700

    Added cors and extra checks

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const hubspot = require("@hubspot/api-client");
+const cors = require("cors");
 const { removeEmpty } = require("./util");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
@@ -9,9 +10,10 @@ const PORT = process.env.PORT || 3000;
 const hsClient = new hubspot.Client({ apiKey: process.env.HUBSPOT_API_KEY });
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Hey" });
+  res.status(201).json({ msg: "Hey" });
 });
 
 app.post("/submit", (req, res) => {
@@ -26,16 +28,14 @@ app.post("/submit", (req, res) => {
     address,
     phone,
   } = req.body;
-
   if (!firstname || !lastname || !email) {
     res.status(401).json({
-      error:
-        "Request body must contain fields 'firstname', 'lastname' and 'email' as a minimum",
+      msg: "Request body must contain fields 'firstname', 'lastname' and 'email' as a minimum",
     });
     return;
   }
 
-  const contactObj = removeEmpty({
+  let contactObj = removeEmpty({
     firstname,
     lastname,
     email,
@@ -53,15 +53,29 @@ app.post("/submit", (req, res) => {
       res.status(201).json({ message: "Contact created successfully" })
     )
     .catch((err) => {
-      console.log("Error making contact object: ", err);
-      res.status(500).json({
-        message:
-          "There was an error making the contact object, check the server logs",
-      });
+      if (err.response.body) {
+        const { category } = err.response.body;
+        if (category == "CONFLICT")
+          res
+            .status(400)
+            .json({ msg: "You have already submitted in our records" });
+        else {
+          console.log("Error making contact object: ", err.response.body);
+
+          res.status(500).json({
+            msg: "There was an error making the contact object, check the server logs",
+          });
+        }
+      } else {
+        console.log("Error making contact object: ", err.response.body);
+
+        res.status(500).json({
+          msg: "There was an error making the contact object, check the server logs",
+        });
+      }
     });
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
-``;
